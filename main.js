adc55095290119aba4cf6c6ad1809cca4da54ac7
wojcikjuliusz-max <wojcikjulek71@gmail.com>
2026-09-22 20:53:06 +0200

    dodałem pasek menu

---
+++ main.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("menu");
+const menu2 = document.getElementById("menu2");
+function wyswietl(){
+    menu.style = "display: none;"
+    menu2.style = "display: block;"
+}
